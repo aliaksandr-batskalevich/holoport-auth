79b59d4f70a8f6c88590184911973ae431f50712
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-10-06 00:13:27 +0300

    init: init express app, added dataBase

--- utils/snakeToCamel.js
+++ utils/snakeToCamel.js
@@ -1,12 +1,13 @@
-const snakeToCamel = (str) => str.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+const snakeToCamel = str => str.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
 
-export const snakeToCamelObj = (obj) => {
+export const snakeToCamelObj = obj => {
     const result = {};
 
-    const keys = Object.keys(obj).map(snakeToCamel);
+    const keys = Object.keys(obj);
     const values = Object.values(obj);
     for (let i = 0; i < keys.length; i++) {
-        result[keys[i]] = values[i];
+        const camelKey = snakeToCamel(keys[i]);
+        result[camelKey] = values[i];
     }
 
     return result;
